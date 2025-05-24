c7a14b09c80e3e4275fd9e9704a0186ac8ba9e1c
Vaigainathan <itsvaigai1211@gmail.com>
2025-05-24 11:37:28 +0530

    Login page fix

--- lib/screens/login/components/login_screen_top_image.dart
+++ lib/screens/login/components/login_screen_top_image.dart
@@ -1,13 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:swap_shelf/components/background.dart';
 import 'package:swap_shelf/screens/login/login_screen.dart';
+import 'package:swap_shelf/screens/signup/signup_page.dart'; // Corrected path
 
-import '../screens/signup/signup_screen.dart';
-
-class Body extends StatelessWidget {
-  const Body({super.key});
+class LoginScreenTopImage extends StatelessWidget {
+  const LoginScreenTopImage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,38 +14,39 @@ class Body extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-              "SWAP SHELF",
-              style: TextStyle(fontWeight: FontWeight.bold),
+          const Text(
+            "SWAP SHELF",
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
           SvgPicture.asset(
-              "assets/icons/chat.svg",
-              height: size.height * 0.3),
+            "assets/icons/chat.svg",
+            height: size.height * 0.3,
+          ),
           SizedBox(
             width: 250,
             child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
+              onPressed: () {
+                Navigator.push(
                   context,
-                      MaterialPageRoute(builder: (context) => LoginScreen())
-                  );
-                },
-                child: Text("LOGIN"),
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+              child: const Text("LOGIN"),
             ),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           SizedBox(
             width: 250,
             child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpScreen())
-                  );
-                },
-                child: Text(
-                  "SIGNUP",
-                ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignUpScreen()),
+                );
+              },
+              child: const Text(
+                "SIGNUP",
+              ),
             ),
           ),
         ],
